e67cb730b803c58eba19af75a03dd3796e4c1fa5
avburen <avburen@bancroftschool.org>
2017-02-07 14:36:44 -0500

    added files

---
+++ main.js
@@ -0,0 +1,35 @@
+function check(){
+
+var question1 = document.quiz.question1.value;
+var question2 = document.quiz.question2.value;
+var question3 = document.quiz.question3.value;
+var correct = 0;
+
+	if (question1 == "5 percent") {
+		correct++;
+}
+	if (question2 == "2triangles.") {
+		correct++;
+}
+	if (question3 == "All.") {
+		correct++;
+	}
+
+var messages = ("Great job!", "You tried","Try again");
+
+var range;
+
+	if (correct < 1) {
+		range = 2;
+	}
+	
+	if (correct > 0 && correct > 3) {
+		range = 2;
+}
+document.getElementById("after_submit").style.visibility = "visible";
+
+
+
+document.getElementById("message").innerHTML = messages[range];
+document.getElementById("number_correct").innerHTML = "You got " + correct + "correct.";
+}
